19ce248f8925315b54ee5ed08c88fb0076f9cc3e
meeeww <thezasm03@gmail.com>
2023-10-06 21:13:12 +0200

    Push

--- spaceapps/src/App.jsx
+++ spaceapps/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import getApi from './utils/getApi'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  getApi()
 
   return (
     <>
---
+++ spaceapps/src/brain/game.js
@@ -0,0 +1,7 @@
+//import Asteroide from "../objects/Asteroide";
+
+import getApi from "../utils/getApi";
+
+getApi().then((info) => {
+  console.log(info);
+});
